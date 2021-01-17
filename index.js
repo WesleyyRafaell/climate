f5d052fe77730ff88a66847125cc57cfabd34d42
rafael <rafaelsanto10s@gmail.com>
2021-01-17 18:47:08 -0300

    primeiro commit

---
+++ index.js
@@ -0,0 +1,27 @@
+const baseUrl = 'https://api.openweathermap.org/data/2.5/weather';
+const apiKey = '8f5e6de9230285a98d0544d469bd972b';
+
+function init(){
+  callApi('São Luís')
+}
+
+
+function callApi(nameCity){
+  fetch(`${baseUrl}?q=${nameCity}&appid=${apiKey}`)
+      .then((response)=> {
+        return response.json();
+      })
+      .then((data)=> {
+        printScreen(data);
+      })
+      .catch((error)=>{
+        console.log('Deu ruim: ', error)
+      })
+}
+
+
+function printScreen(string){
+  console.log(string)
+}
+
+init()
