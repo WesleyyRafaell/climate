b0a29caa9dff89ead9ed9be9570d171653b64994
wesley <rafaelsanto10s@gmail.com>
2021-01-19 09:59:54 -0300

    finalizacao do layout

--- index.js
+++ index.js
@@ -1,27 +1,26 @@
-const baseUrl = 'https://api.openweathermap.org/data/2.5/weather';
-const apiKey = '8f5e6de9230285a98d0544d469bd972b';
+// const baseUrl = 'https://api.openweathermap.org/data/2.5/weather';
+// const apiKey = '8f5e6de9230285a98d0544d469bd972b';
 
-function init(){
-  callApi('São Luís')
-}
+// function init(){
+//   callApi('São Luís')
+// }
+
+// function callApi(nameCity){
+//   fetch(`${baseUrl}?q=${nameCity}&appid=${apiKey}`)
+//       .then((response)=> {
+//         return response.json();
+//       })
+//       .then((data)=> {
+//         printScreen(data);
+//       })
+//       .catch((error)=>{
+//         console.log('Deu ruim: ', error)
+//       })
+// }
 
 
-function callApi(nameCity){
-  fetch(`${baseUrl}?q=${nameCity}&appid=${apiKey}`)
-      .then((response)=> {
-        return response.json();
-      })
-      .then((data)=> {
-        printScreen(data);
-      })
-      .catch((error)=>{
-        console.log('Deu ruim: ', error)
-      })
-}
+// function printScreen(string){
+//   console.log(string)
+// }
 
-
-function printScreen(string){
-  console.log(string)
-}
-
-init()
+// init()
